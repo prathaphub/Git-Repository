c3fcdd5d9ceaf0a164ce06b0a254ccefa71b8c39
Prathap v <54165265+prathaphub@users.noreply.github.com>
2020-08-12 15:59:15 +0530

    Create Traceroute.java
    New file

---
+++ Traceroute.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import traceroute.TracerouteItem;
+public abstract class Traceroute
+{
+private Runtime run;
+public Traceroute()
+{
+run = Runtime.getRuntime();
+}
+public ArrayList<TracerouteItem> traceroute(String destination)
+{
+ArrayList<TracerouteItem> result = new ArrayList<TracerouteItem>();
+Process pr = null;
+String cmd = getTracerouteCommand(destination);
+try
+{
+pr = run.exec(cmd);
+}
+catch(IOException e)
+{
+e.printStackTrace();
+}
+BufferedReader buf = new BufferedReader(new InputStreamReader(
+pr.getInputStream()));
+String line = "";
+try
+{
+while((line = buf.readLine()) != null)
+{
+TracerouteItem item = parse(line);
+result.add(item);
+}
+}
+catch(IOException e)
+{ return null;
+}
+return result;
+}
+public abstract TracerouteItem parse(String line);
+public abstract String getTracerouteCommand(String destination);
+}
+
+
+
